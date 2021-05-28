40338cd8a4b7c5910cc6096219265aebe15d06bb
seddaluca <selucase1997@gmail.com>
2021-05-28 18:40:46 +0200

    Create setup_hadoop.sh

---
+++ spark-terraform/setup_hadoop.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+  hadoop fs -mkdir -p /user/ubuntu
+  hadoop fs -put lemmatization-it.txt
+  hadoop fs -put lexicon-it.csv
+  hadoop fs -put tweet_teams.csv
+  hadoop fs -put tweet_teams_sentiment.csv
+  hadoop fs -put tweet_players.csv
+  hadoop fs -put tweet_players_sentiment.csv
